86e5140eaeed46ecaac063244fba44575c984d97
sanchayata-jain <sanchayata-jain@users.noreply.github.com>
2021-09-09 14:43:25 +0100

    all methods added to ClothingController class

--- src/main/java/com/EthicalClothingShop/EthicalClothing/ClothingLine/ClothingController.java
+++ src/main/java/com/EthicalClothingShop/EthicalClothing/ClothingLine/ClothingController.java
@@ -1,6 +1,49 @@
 package com.EthicalClothingShop.EthicalClothing.ClothingLine;
 
-public class ClothingController {
+import org.springframework.web.bind.annotation.*;
 
-//hi
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/clothes")
+public class ClothingController {
+    private ClothingService clothingService;
+
+    // Constructor
+    public ClothingController(ClothingService clothingService) {
+        this.clothingService = clothingService;
+    }
+
+    //methods
+    @GetMapping
+    public List<ClothingItem> getAllClothingItems() {
+        return clothingService.getClothingItems();
+    }
+
+    @GetMapping("{clothingType}")
+    public List <ClothingItem> getClothingItemsByType(@PathVariable("clothingType") String clothingType) {
+        return clothingService.getClothingItemsOfSameType(clothingType);
+    }
+
+    @GetMapping("{clothingDescription}")
+    public ClothingItem getSingleClothingItem(@PathVariable("clothingDescription") String clothingDescription) {
+        return clothingService.getOneClothingItem(clothingDescription);
+    }
+
+    @PostMapping
+    public void addClothingItem(@RequestBody ClothingItem clothingItem) {
+        clothingService.addNewClothingItem(clothingItem);
+    }
+
+    @DeleteMapping("{clothingDescription}")
+    public void removeClothingItem(@PathVariable("clothingDescription") String clothingDescription) {
+        clothingService.removeOneClothingItem(clothingDescription);
+    }
+
+    @PutMapping
+    public void editClothingItem(@RequestBody ClothingItem clothingItem) {
+        clothingService.updateClothingItem(clothingItem);
+    }
+
+
 }
