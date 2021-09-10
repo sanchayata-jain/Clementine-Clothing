48e5b3f08fd56816c6bd992516774cedadf3c85d
sanchayata-jain <sanchayata-jain@users.noreply.github.com>
2021-09-10 09:08:52 +0100

    created an empty test file for controller

---
+++ src/test/java/com/EthicalClothingShop/EthicalClothing/ClothingLine/ClothingControllerTest.java
@@ -0,0 +1,12 @@
+package com.EthicalClothingShop.EthicalClothing.ClothingLine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClothingControllerTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
