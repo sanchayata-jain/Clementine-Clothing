e7000d267fc45697df322194356f151a266130d5
sanchayata-jain <sanchayata-jain@users.noreply.github.com>
2021-09-10 10:15:24 +0100

    created database access interface and class

--- src/main/java/com/EthicalClothingShop/EthicalClothing/ClothingLine/ClothingDataAccessService.java
+++ src/main/java/com/EthicalClothingShop/EthicalClothing/ClothingLine/ClothingDataAccessService.java
@@ -1,6 +1,24 @@
 package com.EthicalClothingShop.EthicalClothing.ClothingLine;
 
-public class ClothingDataAccessService implements ClothingDAO{
 
+import java.util.ArrayList;
+
+public class ClothingDataAccessService implements ClothingDAO{
+    ArrayList<ClothingItem> clothingItems;
+
+    // constructor
+    public ClothingDataAccessService() {
+        this.clothingItems = new ArrayList<ClothingItem>();
+    }
+
+    @Override
+    public void addClothingItem(ClothingItem clothingItem) {
+        this.clothingItems.add(clothingItem);
+    }
+
+    @Override
+    public void removeClothingItem(ClothingItem clothingItem) {
+        this.clothingItems.remove(clothingItem);
+    }
 
 }
